34d971e25e2d67f2c6f18065f506aec498055fe6
AngelsandDevsLOL <43974738+AngelsandDevsLOL@users.noreply.github.com>
2023-04-09 23:43:50 -0400

    Update GUI.java

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,4 +1,4 @@
-import javax.imageio.ImageIO; // good luck haha
+import javax.imageio.ImageIO; 
 import javax.swing.*;
 import javax.swing.Timer;
 import java.awt.*;
@@ -486,7 +486,7 @@ public class GUI extends Canvas {
 
     public void welcome() throws IOException {
         JFrame frame = new JFrame("Focus Cat");
-        JLabel welcome = new JLabel("TIMER PAGE!");
+        JLabel welcome = new JLabel("WELCOME PAGE!");
         welcome.setFont(new Font("Broadway", Font.BOLD, 30));
         welcome.setBounds(50, 375, 500, 50);
         panel.add(welcome);
